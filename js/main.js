c25fd2866690dc8b695a542f7caef4b2b568f3a7
Yanrong Wang <wang0886@algonquinlive.com>
2023-06-16 13:30:17 -0400

    demo

---
+++ js/main.js
@@ -0,0 +1,28 @@
+console.log("JS file added")
+
+const bodyTag = document.querySelector("body")
+
+bodyTag.innerHTML +=`
+<section id="groceries">
+    <h2>Groceries</h2>
+    <ul id="groceryList"></ul>
+    </section>
+    `
+
+let groceryItems =["apple","bananas","milk","eggs","bread","dog food"]
+const groceryUl= document.getElementById("groceryList")
+
+//what do we want, from we can get it?  // for what coming from where
+for(item of groceryItems){
+    //console.log(item)
+    groceryUl.insertAdjacentHTML("beforeend",`<li>${item}</li>`)
+}
+
+function addItem(newItem){
+    if(newItem !==""){
+     groceryUl.insertAdjacentHTML("afterbegin",`<li>${newItem}</li>`)   
+    }
+
+}
+
+//addItem
